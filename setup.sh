c9052f53ac510e46d3afb2e8073b1f5a6cc3b99c
Oswald De Riemaecker <oswald@continuous.team>
2020-11-15 15:45:31 +0100

    Adding Setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Params: Public IP and Private IP
+# ./setup.sh 100.25.129.241 172.31.65.77
 
-cd $HOME
+HOME_USER=/home/centos
+
+cd $HOME_USER
 git clone https://github.com/oswaldderiemaecker/openstack-training-on-aws.git
 
-cd $HOME/openstack-training-on-aws
-./ip-change.sh 34.224.228.250 $1 172.31.36.180 $2
+cd $HOME_USER/openstack-training-on-aws
+
+chown -R centos:centos $HOME_USER/openstack-training-on-aws
+
+source /root/keystonerc_admin && ./ip-change.sh 34.224.228.250 $1 172.31.36.180 $2
